006424beeb279a1a118a0acae33d12bcbcf033fb
Jiri Konecny <jirka.kon96@gmail.com>
2021-04-03 17:07:01 +0200

    Update EDA

--- R/ExplorationDataAnalysis.R
+++ R/ExplorationDataAnalysis.R
@@ -4,23 +4,24 @@
 
 #Práce se soubory
 
-# Nastavení pracovního adresáøe na složku source file, pøípadne jeji podskožku subfir
+# Nastavení pracovního adresare na slozku source file, pripadne jeji podskozku subdir
 SetWorkingDirectoryToSource <-function(subdir=""){
-  # Nastavení pracovního adresáøe na složku source file
+  # Nastaveni pracovniho adresare na složku source file
   WD <- dirname(rstudioapi::getSourceEditorContext()$path);
-  # Podsložka
+  # Podslozka
   if( subdir != "" )WD <- paste(WD,subdir,sep = "/");
   if (!is.null(WD)) setwd(WD);
+
   return(WD);
 }
 
-#Vrací naètený Excel sheet jako DataFrame
+#Vraci nacteny Excel sheet jako DataFrame
 ReadExcel <-function(file="", sheet="", colNames=NULL){
-  #Naètení dat ve standartním pøedpøipraveném formatu, col_names defaultne naèteny z excelu
+  #Nacteni dat ve standartnim predpripravenem formatu, col_names defaultne nacteny z excelu
   data = read_excel("data_std_raw.xlsx", sheet = "S1")
-  #Pøejmenuji sloupce na použitelné názvy
+  #Prejmenuji sloupce na pouzitelne nazvy
   if( !is.null(colNames) )colnames(data) = colNames;
-  #Convertuji datový typ na dataframe
+  #Konvertuji datovy typ na dataframe
   data = as.data.frame(data);
 
   return(data)
@@ -31,18 +32,19 @@ ReadExcel <-function(file="", sheet="", colNames=NULL){
 #EDA funkce
 
 #Overeni normalniho rozdeleni datasetu
-#Vrací TRUE - je normalniho rozdeli, FALSE = není N
+#Vrací TRUE - je N rozdeli, FALSE = není N
 IsNorm <- function(skewness, kurtosis){
   result = (skewness > -2.0 && skewness < 2.0);
   result = result & (kurtosis > -2.0 && kurtosis < 2.0);
+
   return(result);
 }
 
-#Všechny statistické charkteristiky pro vybraný sloupec -> dplyr
+#Vsechny statisticke charkteristiky datasetu - data, pro vybraný sloupec - colName (pouziti dplyr)
 GetStats <- function(data, colName){
   stats <-  summarise(.data = data,
                       dataLenght = length(.data[[colName]]),
-                      minimum = min(.data[[colName]], na.rm=T),     # preventivní na.rm=T
+                      minimum = min(.data[[colName]], na.rm=T),     # preventivni na.rm=T
                       Q1 = quantile(.data[[colName]], 0.25, na.rm=T),
                       mean = mean(.data[[colName]], na.rm=T),
                       median = median(.data[[colName]], na.rm=T),
@@ -50,15 +52,16 @@ GetStats <- function(data, colName){
                       maximum = max(.data[[colName]], na.rm=T),
                       D = var(.data[[colName]], na.rm=T),
                       sigma = sd(.data[[colName]],na.rm=T),
-                      var_coef_perct = (100*(sigma/mean)),  # variaèní koeficient v procentech
-                      skewness = (moments::skewness(.data[[colName]], na.rm=T)),       # preventivní specifikace balíèku moments
+                      var_coef_perct = (100*(sigma/mean)),  # variacni koeficient v procentech
+                      skewness = (moments::skewness(.data[[colName]], na.rm=T)),       # preventivni specifikace balícku moments
                       kurtosis = (moments::kurtosis(.data[[colName]], na.rm=T)-3),
                       norm = ifelse(IsNorm(skewness, kurtosis), "N", "not-N") #urci zda ma normalni rozdeleni
   )
+
   return(stats);
 }
 
-#Všechny statistické charkteristiky pro vybraný sloupec -> dplyr zhlukovaná pomocí groupBy - groupColName
+#Vsechny statisticke charkteristiky datasetu - data, pro vybraný sloupec - colName, zhlukovaná pomocí groupBy - groupColName (pouziti dplyr)
 GetStatsWithGroupBy <- function(data, colName, groupColName){
   stats <-
     group_by(.data = data, .data[[groupColName]]) %>%
@@ -71,38 +74,39 @@ GetStatsWithGroupBy <- function(data, colName, groupColName){
                       maximum = max(.data[[colName]], na.rm=T),
                       D = var(.data[[colName]], na.rm=T),
                       sigma = sd(.data[[colName]],na.rm=T),
-                      var_coef_perct = (100*(sigma/mean)),  # variaèní koeficient v procentech
-                      skewness = (moments::skewness(.data[[colName]], na.rm=T)),       # preventivní specifikace balíèku moments
+                      var_coef_perct = (100*(sigma/mean)),  # variacni koeficient v procentech
+                      skewness = (moments::skewness(.data[[colName]], na.rm=T)),       # preventivni specifikace balicku moments
                       kurtosis = (moments::kurtosis(.data[[colName]], na.rm=T)-3),
                       norm = ifelse(IsNorm(skewness, kurtosis), "N", "not-N") #urci zda ma normalni rozdeleni
-    )
+              )
+
   return(stats);
 }
 
 
-#Vrací vnitøní hradby DM, HM, a Odlehlá pozorování + BoxPlot
-#Výsledek vrací jako DataFrame, kde Outer jsou Odlehlá pozorování, IQR je IQR, LB je dolní mez, HB je horní mez
+#Vraci vnitrni hradby DM, HM, a Odlehla pozorovani + BoxPlot
+#yýsledek vraci jako DataFrame, kde Outer jsou Odlehla pozorovani, IQR je IQR, LB je dolni mez, HB je horni mez
 GetBordersAndOutValues_AsBoxPlot <- function(data, title="No title", y_name="Y", x_name="", color="lightblue", size=3.0){
   #Omezeni
   sigma = sd(data,na.rm=T);
   limMin = min(data) - size*sigma; #min
   limMax = max(data) + size*sigma; #max
 
-  #Odlehlé hodnoty a boxplot
+  #Odlehle hodnoty a boxplot
   boxplt = boxplot(data,
                    main = title,
                    ylab = y_name,
                    xlab = x_name,
                    col = color,
                    ylim= c(limMin,limMax))
-  #Odlehlá pozorování
+  #Odlehla pozorovani
   outer = boxplt$out
 
-  # Použití vnitøních hradeb - obecnìjší postup - uvedeno bez ohledu na výrobce!!!
+  # Pouziti vnitrnich hradeb - obecnejsí postup
   IQR = IQR(data)
 
-  LB = quantile(data, 0.25, na.rm=T) - 1.5*IQR  # výpoèet dolní mezi vnitøních hradeb
-  HB = quantile(data, 0.75, na.rm=T) + 1.5*IQR  # výpoèet horní mezi vnitøních hradeb
+  LB = quantile(data, 0.25, na.rm=T) - 1.5*IQR  # vypocet dolni mezi vnitrnich hradeb
+  HB = quantile(data, 0.75, na.rm=T) + 1.5*IQR  # vypocet horni mezi vnitrnich hradeb
 
   result <- matrix(c(outer,IQR,LB,HB),ncol=4,byrow=TRUE);
   colnames(result) <- c("Outer","IQR","LB","HB");
